2d1e34d0ee4feaf3f6e466f119f03c02dbaf2126
Gennady Basov <g.a.basov@gmail.com>
2016-10-12 20:20:15 +0300

    fix typo

--- src/Jenssegers/Mongodb/Query/Builder.php
+++ src/Jenssegers/Mongodb/Query/Builder.php
@@ -911,7 +911,7 @@ class Builder extends BaseBuilder
 
             // Convert DateTime values to UTCDateTime.
             if (isset($where['value']) and $where['value'] instanceof DateTime) {
-                $where['value'] = UTCDateTime(
+                $where['value'] = new UTCDateTime(
                     $where['value']->getTimestamp() * 1000 +
                     intval(intval($where['value']->format('u')) / 1000)
                 );
