996cff1b456b46377b50c6bee17ce154140e72b1
Gennady Basov <g.a.basov@gmail.com>
2017-02-21 15:19:09 +0300

    remove unrelated comment

--- src/Jenssegers/Mongodb/Schema/Grammar.php
+++ src/Jenssegers/Mongodb/Schema/Grammar.php
@@ -2,11 +2,7 @@
 namespace Jenssegers\Mongodb\Schema;
 
 use Illuminate\Database\Schema\Grammars\Grammar as BaseGrammar;
-/**
- * Class Grammar
- *
- * @package Moloquent\Schema
- */
+
 class Grammar extends BaseGrammar
 {
 }
